bd203c59e72e8115c71cefdb2f438c522912a0ae
TotoroDev <ilushka.tab@gmail.com>
2026-05-12 00:05:45 +0300

    Project base skeleton

---
+++ src/main/java/com/thetotorodev/dictionary_users_api/DictionaryUsersApiApplication.java
@@ -0,0 +1,13 @@
+package com.thetotorodev.dictionary_users_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryUsersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionaryUsersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thetotorodev/dictionary_users_api/DictionaryUsersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thetotorodev.dictionary_users_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryUsersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
